94ed6dfefa603ee575be7a6bb57f16ac3cd669c3
KayahMcVay <kayahjosephine@gmail.com>
2025-05-12 23:19:44 -0400

    Completed Phase 1

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,74 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private ArrayList<Vehicle> inventory;
+
+
+    public Dealership(String name, String address, String phoneNumber, ArrayList<Vehicle> inventory){
+
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.inventory = inventory;
+
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(){
+
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(){
+
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByYear(){
+
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByColor(){
+
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByMileage(){
+
+        return null;
+    }
+
+
+    public ArrayList<Vehicle> getVehiclesByType(){
+
+        return null; 
+    }
+
+
+    public ArrayList<Vehicle> getAllVehicles(){
+
+        return null;
+    }
+
+
+    public void addVehicles(){
+
+    }
+
+
+    public void removeVehicles(){
+
+
+    }
+
+
+}
